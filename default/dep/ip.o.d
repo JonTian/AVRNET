de271f6526701f38d175894d51911b033c7dcc95
JonTian <oievetian@gmail.com>
2016-06-22 05:21:34 +0800

    Signed-off-by: JonTian <oievetian@gmail.com>

---
+++ default/dep/ip.o.d
@@ -0,0 +1,80 @@
+ip.o: ../ip.c ../includes.h \
+  c:/winavr-20070525/bin/../avr/include/avr/io.h \
+  c:/winavr-20070525/bin/../avr/include/avr/sfr_defs.h \
+  c:/winavr-20070525/bin/../avr/include/inttypes.h \
+  c:/winavr-20070525/bin/../avr/include/stdint.h \
+  c:/winavr-20070525/bin/../avr/include/avr/iom32.h \
+  c:/winavr-20070525/bin/../avr/include/avr/portpins.h \
+  c:/winavr-20070525/bin/../avr/include/avr/version.h \
+  c:/winavr-20070525/bin/../avr/include/stdio.h \
+  c:\winavr-20070525\bin\../lib/gcc/avr/4.1.2/include/stdarg.h \
+  c:\winavr-20070525\bin\../lib/gcc/avr/4.1.2/include/stddef.h \
+  c:/winavr-20070525/bin/../avr/include/stdlib.h \
+  c:/winavr-20070525/bin/../avr/include/string.h \
+  c:/winavr-20070525/bin/../avr/include/util/delay.h \
+  c:/winavr-20070525/bin/../avr/include/util/delay_basic.h \
+  c:/winavr-20070525/bin/../avr/include/avr/pgmspace.h \
+  c:/winavr-20070525/bin/../avr/include/avr/eeprom.h \
+  c:/winavr-20070525/bin/../avr/include/avr/wdt.h ../struct.h \
+  ../enc28j60.h ../ethernet.h ../ip.h ../tcp.h ../icmp.h ../arp.h \
+  ../udp.h ../http.h ../lcd.h ../menu.h ../adc.h
+
+../includes.h:
+
+c:/winavr-20070525/bin/../avr/include/avr/io.h:
+
+c:/winavr-20070525/bin/../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20070525/bin/../avr/include/inttypes.h:
+
+c:/winavr-20070525/bin/../avr/include/stdint.h:
+
+c:/winavr-20070525/bin/../avr/include/avr/iom32.h:
+
+c:/winavr-20070525/bin/../avr/include/avr/portpins.h:
+
+c:/winavr-20070525/bin/../avr/include/avr/version.h:
+
+c:/winavr-20070525/bin/../avr/include/stdio.h:
+
+c:\winavr-20070525\bin\../lib/gcc/avr/4.1.2/include/stdarg.h:
+
+c:\winavr-20070525\bin\../lib/gcc/avr/4.1.2/include/stddef.h:
+
+c:/winavr-20070525/bin/../avr/include/stdlib.h:
+
+c:/winavr-20070525/bin/../avr/include/string.h:
+
+c:/winavr-20070525/bin/../avr/include/util/delay.h:
+
+c:/winavr-20070525/bin/../avr/include/util/delay_basic.h:
+
+c:/winavr-20070525/bin/../avr/include/avr/pgmspace.h:
+
+c:/winavr-20070525/bin/../avr/include/avr/eeprom.h:
+
+c:/winavr-20070525/bin/../avr/include/avr/wdt.h:
+
+../struct.h:
+
+../enc28j60.h:
+
+../ethernet.h:
+
+../ip.h:
+
+../tcp.h:
+
+../icmp.h:
+
+../arp.h:
+
+../udp.h:
+
+../http.h:
+
+../lcd.h:
+
+../menu.h:
+
+../adc.h:
